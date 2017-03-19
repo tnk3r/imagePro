383174e2f727fdf7adbef7956c7039b17cd9b9df
metalworker <abcfilm@gmail.com>
2017-03-19 15:56:41 -0700

    Simplified

--- captureVideo.py
+++ captureVideo.py
@@ -1,31 +1,18 @@
-import cv2
-import sys, os, time
-#### WEBCAM ####
-#
-# if sys.argv[1] == 'webcam':
-#     cap = cv2.VideoCapture(0)
-# else:
-#     cap = cv2.VideoCapture(sys.argv[1])
+import cv2, imutils
+import numpy as np
 
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture(sys.argv[1])
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# out = cv2.VideoWriter(sys.argv[2],fourcc, 24.0, (640,480))
+cap = cv2.VideoCapture(0)
+fps = cap.get(cv2.CAP_PROP_FPS)
 
+angle = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
-    # fet, frame2 = img.read()
     if ret == True:
-        # frame2 = cv2.resize(frame2, (600, 480))
-        # dst = cv2.addWeighted(frame,0.7,frame2,0.3,0)
-        # frame = cv2.flip(frame, 0)
-        # out.write(frame)
+    	rotated = imutils.rotate_bound(frame, angle)
+    	cv2.imshow("Rotated (Correct)", rotated)
+        angle+=45
+    cv2.waitKey(5)
 
-        cv2.imshow('frame',dst)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
+
 cap.release()
-out.release()
 cv2.destroyAllWindows()
--- openVideo.py
+++ openVideo.py
@@ -1,37 +1,22 @@
 import numpy as np
 import cv2, sys
 
-# opens a webcam with -w or opens a file as 1st arg
 if sys.argv[1] == "-w":
     src = cv2.VideoCapture(0)
 else:
     src = cv2.VideoCapture(sys.argv[1])
 
-distCoeff = np.zeros((4,1),np.float64)
-
-k1 = -1.0e-5; # warping
-k2 = 0.0;
-p1 = 0.0;
-p2 = 0.0;
-
-distCoeff[0,0] = k1;
-distCoeff[1,0] = k2;
-distCoeff[2,0] = p1;
-distCoeff[3,0] = p2;
-
-cam = np.eye(3,dtype=np.float32)
 
 while True:
     ret, frame = src.read()
-    width, height, ch = frame.shape
+    ## Half the Size // double the proc speed
+    height, width = frame.shape[:2]
+    res = cv2.resize(frame,(width/2, height/2), interpolation = cv2.INTER_CUBIC)
 
-    cam[0,2] = width/2.0  # define center x
-    cam[1,2] = height/2.0 # define center y
-    cam[0,0] = 10.        # define focal length x
-    cam[1,1] = 10.        # define focal length y
 
-    dst = cv2.undistort(frame,cam,distCoeff)
+    cv2.imshow('frame',res)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    cv2.imshow('dst',dst)
-    cv2.waitKey(5)
+src.release()
 cv2.destroyAllWindows()
